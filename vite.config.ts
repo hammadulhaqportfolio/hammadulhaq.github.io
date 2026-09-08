73c64bd0b57cb677c12a49c534f8d43188f9ab02
hammadulhaq <sportsfactspk@gmail.com>
2026-09-08 15:24:47 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/hammadulhaq.github.io/",
-  
+  vite: {
+    base: "/hammadulhaq.github.io/",
+  },
+
   tanstackStart: {
     prerender: {
       enabled: true,
